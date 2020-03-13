0f2f8dc2d6f1ea03f56af2c6443da09e8068b649
Ozaki Yoichi <zak@inl.ics.keio.ac.jp>
2020-03-13 18:04:28 +0900

    abc122d

---
+++ atcoder/abc122d.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// ACをまとめて扱うと，「TTTA|CTTACTT|TTTT」みたいな文字列で答えが合わないので
+// 「右隣にCがあるようなA」の個数をカウントすると問題を読み替える
+// さらに「TTACTTA|C」みたいな右端のAの右隣にCがあるようなときのAを除外するために区間[l,r)を考える
+
+int main() {
+    int N, Q; cin >> N >> Q;
+    string str; cin >> str;
+
+    vector<int> s(N+1, 0); // s[i]: i文字目までに存在する「右隣にCがあるようなA」の個数
+    for (int i = 0; i < N; i++) {
+        if (i+1 < N && str[i] == 'A' && str[i+1] == 'C') {
+            s[i+1] = s[i] + 1;
+        } else {
+            s[i+1] = s[i];
+        }
+    }
+    for (int i = 0; i < N+1; i++) {
+        cout << s[i] << endl;
+    }
+    for (int q = 0; q < Q; q++) {
+        int l, r; cin >> l >> r;
+        l--; r--;
+        cout << s[r] - s[l] << endl;
+    }
+    return 0;
+}
