42186d3e2c04cab2855fe58611f069f4310a0a0b
Ozaki Yoichi <zak@inl.ics.keio.ac.jp>
2020-05-08 18:07:21 +0900

    abc150a

---
+++ atcoder/abc150a.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int K, X;
+    cin >> K >> X;
+    if (X <= K * 500)
+        cout << "Yes" << endl;
+    else
+        cout << "No" << endl;
+    return 0;
+}
