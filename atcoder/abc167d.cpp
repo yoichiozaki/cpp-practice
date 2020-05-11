4fac7e83511e0d825316d3591f618cf5e007d821
Ozaki Yoichi <zak@inl.ics.keio.ac.jp>
2020-05-11 13:39:01 +0900

    abc167d

--- atcoder/abc167d.cpp
+++ atcoder/abc167d.cpp
@@ -4,13 +4,36 @@ using ll = long long;
 
 int main()
 {
-    ll N, K;
-    cin >> N >> K;
-    vector<ll> A(N);
-    for (ll i = 0; i < N; i++)
+    int n;
+    ll k;
+    cin >> n >> k;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+
+    vector<int> s;
+    vector<int> ord(n + 1, -1);
+
+    int cycle = 1, leg = 0;
     {
-        cin >> A[i];
+        int now = 1;
+        while (ord[now] == -1)
+        { // nowが未訪問ならwhileに入る
+            ord[now] = s.size();
+            s.push_back(now);
+            now = a[now - 1];
+        }
+        cycle = s.size() - ord[now];
+        leg = ord[now];
     }
 
+    if (k < leg)
+        cout << s[k] << endl;
+    else
+    {
+        k -= leg;
+        k %= cycle;
+        cout << s[leg + k] << endl;
+    }
     return 0;
 }
