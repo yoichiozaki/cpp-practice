5124f490a5728893b43232fe69c3e66e9c6b9dc4
Ozaki Yoichi <zak@inl.ics.keio.ac.jp>
2020-05-28 16:16:49 +0900

    dp template

---
+++ dp.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+template <class T>
+inline bool chmax(T &a, T b)
+{
+    if (a < b)
+    {
+        a = b;
+        return 1;
+    }
+    return 0;
+}
+template <class T>
+inline bool chmin(T &a, T b)
+{
+    if (a > b)
+    {
+        a = b;
+        return 1;
+    }
+    return 0;
+}
+
+int main()
+{
+    return 0;
+}
