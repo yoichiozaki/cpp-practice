f6ec4282cb4a67b12f2bccfbb7761910d7ce4dea
Ozaki Yoichi <zak@inl.ics.keio.ac.jp>
2020-06-08 14:52:08 +0900

    Revert "past"
    This reverts commit 541c75bca1850c94473b009dc86191bf148974d9.

--- atcoder/tempCodeRunnerFile.cpp
+++ atcoder/tempCodeRunnerFile.cpp
@@ -3,47 +3,25 @@ using namespace std;
 
 int main()
 {
-    long long N;
-    cin >> N;
-    vector<long long> R(N), C(N);
-    for (long long i = 0; i < N; i++)
-        R[i] = C[i] = i;
-    bool side = true; // N*R + C
-    long long Q;
-    cin >> Q;
-    for (long long q = 0; q < Q; q++)
+    int N;
+    long long K;
+    cin >> N >> K;
+    vector<long long> csum;
+    csum.push_back(0LL);
+    long long total = 0LL;
+    for (int i = 0; i < N; i++)
     {
-        long long type;
-        cin >> type;
-        if (type == 1)
-        {
-            long long A, B;
-            cin >> A >> B;
-            A--, B--;
-            swap(C[A], C[B]);
-        }
-        else if (type == 2)
-        {
-            long long A, B;
-            cin >> A >> B;
-            A--, B--;
-            swap(R[A], R[B]);
-        }
-        else if (type == 3)
-        {
-            side = !side;
-            swap(R, C);
-        }
-        else
-        {
-            long long A, B;
-            cin >> A >> B;
-            A--, B--;
-            if (side)
-                cout << N * R[A] + C[B] << endl;
-            else
-                cout << N * C[B] + R[A] << endl;
-        }
+        long long a;
+        cin >> a;
+        total += a;
+        csum.push_back(total);
     }
+
+    long long cnt = 0LL;
+    for (int i = 0; i < N; i++)
+    {
+        cnt += csum.end() - lower_bound(csum.begin() + i + 1, csum.end(), csum[i] + K);
+    }
+    cout << cnt << endl;
     return 0;
 }
