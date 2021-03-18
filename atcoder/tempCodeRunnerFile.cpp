a9557ae4c5f7f395363cc2cd67b48e4288483fcc
Ozaki Yoichi <zak@inl.ics.keio.ac.jp>
2021-03-18 10:09:10 +0000

    abc

--- atcoder/tempCodeRunnerFile.cpp
+++ atcoder/tempCodeRunnerFile.cpp
@@ -3,28 +3,6 @@ using namespace std;
 
 int main()
 {
-    int A, B, C;
-    cin >> A >> B >> C;
-    if (C == 0)
-    {
-        if (A % 2 == 0 && B % 2 == 0)
-            cout << "Aoki" << endl;
-        else if (A % 2 == 0 && B % 2 == 1)
-            cout << "Takahashi" << endl;
-        else if (A % 2 == 1 && B % 2 == 0)
-            cout << "Takahashi" << endl;
-        else
-            cout << "Aoki" << endl;
-    }
-    else // C == 1
-    {
-        if (A % 2 == 0 && B % 2 == 0)
-            cout << "Takahashi" << endl;
-        else if (A % 2 == 0 && B % 2 == 1)
-            cout << "Aoki" << endl;
-        else if (A % 2 == 1 && B % 2 == 0)
-            cout << "Aoki" << endl;
-        else
-            cout << "Takahashi" << endl;
-    }
+    double A, B; cin >> A >> B;
+    cout << (A - B) / A << endl;
 }
