f7c0801ab12807d064f45e3e8b5536239efbc945
Ozaki Yoichi <zak@inl.ics.keio.ac.jp>
2021-06-27 04:32:51 +0000

    past202012d

---
+++ atcoder/past202012d.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int N;
+    cin >> N;
+    vector<tuple<long long, long long, string>> table(N);
+    for (int i = 0; i < N; i++)
+    {
+        string S;
+        cin >> S;
+        long long j = 0;
+        long long k = 0;
+        while (S[j] == '0')
+        {
+            j++;
+            k--;
+        }
+        long long d = 0;
+        for (; j < S.size(); j++)
+            d = d * 10LL + S[j] - '0';
+        table[i] = make_tuple(d, k, S);
+    }
+    sort(table.begin(), table.end());
+    for (int i = 0; i < N; i++)
+        cout << get<0>(table[i]) << " " << get<1>(table[i]) << " " << get<2>(table[i]) << endl;
+    return 0;
+}
