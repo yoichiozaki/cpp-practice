c9759d994a676ade7c77b7f9ca81793212070509
Ozaki Yoichi <zak@inl.ics.keio.ac.jp>
2021-06-27 03:09:01 +0000

    abc207b

---
+++ atcoder/abc207b.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    long long A, B, C, D;
+    cin >> A >> B >> C >> D;
+    long long blue = A, red = 0;
+    for (long long i = 1; i <= A; i++)
+    {
+        blue += B;
+        red += C;
+        if (blue <= D * red)
+        {
+            cout << i << endl;
+            return 0;
+        }
+    }
+    cout << -1 << endl;
+    return 0;
+}
