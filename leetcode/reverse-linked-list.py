52e7d813abb01aca047613a7375f1b9135603401
Ozaki Yoichi <zak@inl.ics.keio.ac.jp>
2021-06-30 08:27:55 +0000

    add iterative solution

--- leetcode/reverse-linked-list.py
+++ leetcode/reverse-linked-list.py
@@ -11,3 +11,21 @@ class Solution:
         head.next.next = head
         head.next = None
         return reversed_head
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+
+class Solution:
+    def reverseList(self, head: ListNode) -> ListNode:
+        prev = None
+        curr = head
+        while curr is not None:
+            tmp = curr.next
+            curr.next = prev
+            prev = curr
+            curr = tmp
+        return prev
