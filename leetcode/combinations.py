8a679a2500d619fbb222b219098b6f5ed3ffc1eb
Ozaki Yoichi <zak@inl.ics.keio.ac.jp>
2021-07-20 12:52:55 +0000

    combinations.py

---
+++ leetcode/combinations.py
@@ -0,0 +1,12 @@
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        ret = []
+
+        def rec(nums, sofar):
+            if len(sofar) == k:
+                ret.append(sofar)
+                return
+            for i in range(len(nums)):
+                rec(nums[i+1:], sofar + [nums[i]])
+        rec([i+1 for i in range(n)], [])
+        return ret
