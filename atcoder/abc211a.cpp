b9202fdffb545271a4094cdf26274c591fb8c48e
Ozaki Yoichi <zak@inl.ics.keio.ac.jp>
2021-07-25 12:17:48 +0000

    abc211a

---
+++ atcoder/abc211a.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    double A, B;
+    cin >> A >> B;
+    cout << setprecision(12) << (A - B) / 3.0 + B << endl;
+    return 0;
+}
