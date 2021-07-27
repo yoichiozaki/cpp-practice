ccc3395b5e0a73b39b979ae0d8a42a3f395bcecb
Ozaki Yoichi <zak@inl.ics.keio.ac.jp>
2021-07-27 02:06:30 +0000

    coin-change.py

--- leetcode/coin-change.py
+++ leetcode/coin-change.py
@@ -14,3 +14,15 @@ class Solution:
                 else:
                     dp[i][j] = dp[i-1][j]
         return dp[-1][-1] if dp[-1][-1] != INF else -1
+
+
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        # dp[i]: i円支払うのに必要な硬貨の最小枚数
+        dp = [float("inf") for _ in range(1 + amount)]
+        dp[0] = 0
+        for i in range(1, 1 + amount):
+            for coin in coins:
+                if 0 <= i - coin:
+                    dp[i] = min(dp[i], dp[i - coin] + 1)
+        return dp[amount] if dp[amount] != float("inf") else -1
