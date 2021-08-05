0d36438ef745c378a9b480bf056d12232cb856f0
Ozaki Yoichi <zak@inl.ics.keio.ac.jp>
2021-08-05 07:16:31 +0000

    past202107i

--- atcoder/past202107i.cpp
+++ atcoder/past202107i.cpp
@@ -16,6 +16,7 @@ int main()
     long double E = sqrt(x2 * x2 + y2 * y2);
     long double cos = x2 / E;
     long double sin = -y2 / E;
+    cout << setprecision(12);
     for (int i = 0; i < N; i++)
     {
         long double x, y;
