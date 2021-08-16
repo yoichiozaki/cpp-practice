b4a7e4364c66a64b5a05239620ea115a8b565394
Ozaki Yoichi <zak@inl.ics.keio.ac.jp>
2021-08-16 06:17:51 +0000

    abc214b

---
+++ atcoder/abc214b.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int S, T;
+    cin >> S >> T;
+    int ans = 0;
+    for (int a = 0; a <= S; a++)
+    {
+        for (int b = 0; a + b <= S; b++)
+        {
+            for (int c = 0; a + b + c <= S; c++)
+            {
+                if (a * b * c <= T)
+                    ans += 1;
+            }
+        }
+    }
+    cout << ans << endl;
+    return 0;
+}
