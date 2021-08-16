d506422eb222df58975ffec415fa835567b66aad
Ozaki Yoichi <zak@inl.ics.keio.ac.jp>
2021-08-16 07:40:37 +0000

    combination-sum4.py

--- leetcode/combination-sum4.py
+++ leetcode/combination-sum4.py
@@ -1,3 +1,33 @@
+# すぐに思いつきたい再帰な解法
+class Solution:
+    def combinationSum4(self, nums: List[int], target: int) -> int:
+        if target == 0:
+            return 1
+        ans = 0
+        for num in nums:
+            if num <= target:
+                ans += self.combinationSum4(nums, target - num)
+        return ans
+
+
+# トップダウンな解法：メモ化
+class Solution:
+    def combinationSum4(self, nums: List[int], target: int) -> int:
+        memo = [1] + [-1 for _ in range(target)]
+
+        def rec(nums, target):
+            if memo[target] != -1:
+                return memo[target]
+            ans = 0
+            for num in nums:
+                if num <= target:
+                    ans += rec(nums, target - num)
+            memo[target] = ans
+            return ans
+        return rec(nums, target)
+
+
+# ボトムアップな解法
 class Solution:
     def combinationSum4(self, nums: List[int], target: int) -> int:
         # dp[i]: 和がiになる組み合わせの場合の数
@@ -14,3 +44,5 @@ class Solution:
         return dp[target]
 
 # follow-up question: numsに負の数が許される場合は？
+
+# follow-up question: coin change問題との類似性
