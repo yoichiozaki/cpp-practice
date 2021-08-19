bb5235c0123492c1e827b1fa3c9d3492f642b117
Ozaki Yoichi <zak@inl.ics.keio.ac.jp>
2021-08-19 02:53:18 +0000

    number-of-longest-increasing-subsequence.py

---
+++ leetcode/number-of-longest-increasing-subsequence.py
@@ -0,0 +1,28 @@
+class Solution:
+    def findNumberOfLIS(self, nums: List[int]) -> int:
+        # length[i]: 右端がnums[i]である最長増加部分列の長さ
+        # count[i] : 右端がnums[i]である最長増加部分列の個数
+        # 求めるべきは count[k]．ただし k は length の最大値のインデックス
+
+        n = len(nums)
+        length = [1 for _ in range(n)]
+        count = [1 for _ in range(n)]
+        max_length = 0
+        ans = 0
+
+        for i in range(n):
+            for j in range(0, i):
+                if nums[j] < nums[i]:
+                    if length[j] + 1 == length[i]:
+                        count[i] += count[j]
+                    if length[j] + 1 > length[i]:
+                        length[i] = length[j] + 1
+                        count[i] = count[j]
+
+            if max_length == length[i]:
+                ans += count[i]
+            if max_length < length[i]:
+                max_length = length[i]
+                ans = count[i]
+
+        return ans
