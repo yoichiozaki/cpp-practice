28fb5a797516b68321923d3494c4c3caad60dbf6
Ozaki Yoichi <zak@inl.ics.keio.ac.jp>
2021-08-23 05:57:01 +0000

    reverse-linked-list-2.py

--- leetcode/reverse-linked-list-2.py
+++ leetcode/reverse-linked-list-2.py
@@ -4,20 +4,21 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def reverseBetween(self, head: ListNode, left: int, right: int) -> ListNode:
+    def reverseBetween(self, head: Optional[ListNode], left: int, right: int) -> Optional[ListNode]:
         if head is None:
             return head
 
         prev = None
         curr = head
+
         while 1 < left:
             prev = curr
             curr = curr.next
             left -= 1
             right -= 1
 
-        tail = curr
-        conn = prev
+        connection = prev
+        reversed_tail = curr
 
         while 0 < right:
             tmp = curr.next
@@ -26,10 +27,10 @@ class Solution:
             curr = tmp
             right -= 1
 
-        if conn is not None:
-            conn.next = prev
+        if connection is not None:
+            connection.next = prev
         else:
             head = prev
+        reversed_tail.next = curr
 
-        tail.next = curr
         return head
