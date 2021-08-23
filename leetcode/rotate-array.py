a61f2dd339c91293171fa1acb21ba83da0906d0b
Ozaki Yoichi <zak@inl.ics.keio.ac.jp>
2021-08-23 10:42:53 +0000

    rotate-array.py

---
+++ leetcode/rotate-array.py
@@ -0,0 +1,27 @@
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        def reverse(nums, left, right):
+            while left < right:
+                nums[left], nums[right] = nums[right], nums[left]
+                left += 1
+                right -= 1
+
+        # [1, 2, 3, 4, 5] を 7 シフト
+        # [5, 4, 3, 2, 1] -> [4, 5] + [1, 2, 3] -> [4, 5, 1, 2, 3]
+
+        k %= len(nums)
+        reverse(nums, 0, len(nums) - 1)
+        reverse(nums, 0, k - 1)
+        reverse(nums, k, len(nums) - 1)
+
+
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        k %= len(nums)
+        nums[:] = nums[-k:] + nums[:-k]
