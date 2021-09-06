10df023cc920f8d594e157bc47f7a474c82f213f
Ozaki Yoichi <zak@inl.ics.keio.ac.jp>
2021-09-06 23:37:10 +0000

    k-th-smallest-element-in-a-bst.py

---
+++ leetcode/k-th-smallest-element-in-a-bst.py
@@ -0,0 +1,38 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def kthSmallest(self, root: TreeNode, k: int) -> int:
+        inordered = []
+
+        def inorder(root):
+            if root is None:
+                return
+            inorder(root.left)
+            inordered.append(root.val)
+            inorder(root.right)
+        inorder(root)
+        return inordered[k - 1]
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def kthSmallest(self, root: TreeNode, k: int) -> int:
+        stack = []
+        while True:
+            while root is not None:
+                stack.append(root)
+                root = root.left
+            root = stack.pop()
+            k -= 1
+            if k == 0:
+                return root.val
+            root = root.right
