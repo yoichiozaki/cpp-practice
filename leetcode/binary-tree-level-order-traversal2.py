65f9a32ca9639192c92897a7e908bf27f8835e33
Ozaki Yoichi <zak@inl.ics.keio.ac.jp>
2021-09-20 06:22:22 +0000

    binary-tree-level-order-traversal2.py

---
+++ leetcode/binary-tree-level-order-traversal2.py
@@ -0,0 +1,32 @@
+# BFS
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def levelOrderBottom(self, root: TreeNode) -> List[List[int]]:
+        if root is None:
+            return []
+
+        suspended = []
+        suspended.append(root)
+
+        lsts = []
+        level = 0
+
+        while len(suspended) != 0:
+            if len(lsts) == level:
+                lsts.append([])
+            next_suspended = []
+            for u in suspended:
+                lsts[level].append(u.val)
+                if u.left is not None:
+                    next_suspended.append(u.left)
+                if u.right is not None:
+                    next_suspended.append(u.right)
+            suspended = next_suspended
+            level += 1
+
+        return lsts[::-1]
