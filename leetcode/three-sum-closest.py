53dcc9ffb885e1df3fb405ca8a459061d3a54916
Ozaki Yoichi <zak@inl.ics.keio.ac.jp>
2021-10-01 03:21:30 +0000

    three-sum-closest.py

---
+++ leetcode/three-sum-closest.py
@@ -0,0 +1,18 @@
+class Solution:
+    def threeSumClosest(self, nums: List[int], target: int) -> int:
+        diff = float("inf")
+        nums.sort()
+        for i in range(len(nums) - 2):
+            left = i + 1
+            right = len(nums) - 1
+            while left < right:
+                three_sum = nums[i] + nums[left] + nums[right]
+                if abs(target - three_sum) < abs(diff):
+                    diff = target - three_sum
+                if three_sum < target:
+                    left += 1
+                else:
+                    right -= 1
+            if diff == 0:
+                break
+        return target - diff
