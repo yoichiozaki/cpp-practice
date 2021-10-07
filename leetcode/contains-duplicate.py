214abcc5c5e15a6c7d249d9e8d219ae57ecc235d
Ozaki Yoichi <zak@inl.ics.keio.ac.jp>
2021-10-07 08:48:45 +0000

    contains-duplicate.py

--- leetcode/contains-duplicate.py
+++ leetcode/contains-duplicate.py
@@ -6,3 +6,13 @@ class Solution:
                 return True
             s.add(num)
         return False
+
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        nums.sort()
+        for i in range(1, len(nums)):
+            if nums[i-1] == nums[i]:
+                return True
+
+        return False
