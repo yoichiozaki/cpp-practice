b886a4883f5f83551a6b05bb75cc46b0bc7a5991
Ozaki Yoichi <zak@inl.ics.keio.ac.jp>
2021-10-09 10:33:48 +0000

    sliding-window-maximum.py

--- leetcode/sliding-window-maximum.py
+++ leetcode/sliding-window-maximum.py
@@ -28,3 +28,38 @@ class Solution:
             if k - 1 <= i:
                 ans.append(nums[q[0]])
         return ans
+
+
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        class MonoQueue:
+            def __init__(self):
+                self.que = collections.deque()
+
+            def push(self, val):
+                count = 0
+                while len(self.que) != 0 and self.que[-1][0] < val:
+                    count += self.que[-1][1] + 1
+                    self.que.pop()
+                self.que.append([val, count])
+
+            def max(self):
+                return self.que[0][0]
+
+            def pop(self):
+                if 0 < self.que[0][1]:
+                    self.que[0][1] -= 1
+                    return
+                else:
+                    self.que.popleft()
+
+        ans = []
+        mq = MonoQueue()
+        k = min(k, len(nums))
+        for i in range(0, k - 1):
+            mq.push(nums[i])
+        for i in range(k - 1, len(nums)):
+            mq.push(nums[i])
+            ans.append(mq.max())
+            mq.pop()
+        return ans
