bca9468503c52755eb5410d06d4cc5574578068c
Ozaki Yoichi <zak@inl.ics.keio.ac.jp>
2021-10-24 09:24:19 +0000

    abc224a

---
+++ atcoder/abc224a.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    string S;
+    cin >> S;
+    if (S[S.length() - 1] == 't')
+        cout << "ist" << endl;
+    else
+        cout << "er" << endl;
+    return 0;
+}
