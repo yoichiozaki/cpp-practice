3d3c3cc358a7daf084c99e025177b5b631557d98
Ozaki Yoichi <zak@inl.ics.keio.ac.jp>
2022-02-07 14:26:45 +0000

    abc238a

---
+++ atcoder/abc238a.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    if (2 <= n && n <= 4)
+        cout << "No" << endl;
+    else
+        cout << "Yes" << endl;
+    return 0;
+}
