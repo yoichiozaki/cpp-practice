05e0dd36755abce5f9ad562e858e2de2be21d329
Ozaki Yoichi <zak@inl.ics.keio.ac.jp>
2022-02-10 04:23:25 +0000

    dwacon5a

--- atcoder/dwacon5a.cpp
+++ atcoder/dwacon5a.cpp
@@ -1,9 +1,27 @@
-TODO :
 #include <bits/stdc++.h>
-    using namespace std;
+using namespace std;
 
 int main()
 {
-
+    int N;
+    cin >> N;
+    vector<int> A(N);
+    int s = 0;
+    for (int i = 0; i < N; i++)
+    {
+        cin >> A[i];
+        s += A[i];
+    }
+    int diff = INT_MAX / 2;
+    int ans = N + 1;
+    for (int i = 0; i < N; i++)
+    {
+        if (abs(s - N * A[i]) < diff)
+        {
+            diff = abs(s - N * A[i]);
+            ans = i;
+        }
+    }
+    cout << ans << endl;
     return 0;
 }
