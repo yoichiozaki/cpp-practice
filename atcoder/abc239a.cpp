4fc2909aa4906c07e63e79cb57f6ccf941d96d96
Ozaki Yoichi <zak@inl.ics.keio.ac.jp>
2022-02-22 08:09:26 +0000

    update

---
+++ atcoder/abc239a.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    double H;
+    cin >> H;
+    cout << setprecision(12) << sqrt(H * (12800000.0 + H)) << endl;
+    return 0;
+}
