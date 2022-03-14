0ccb4f6e7b831aa41eb786a5d530f9e4a705e9a2
Ozaki Yoichi <zak@inl.ics.keio.ac.jp>
2022-03-14 05:02:01 +0000

    leetcode

---
+++ leetcode/543_diameter_of_binary_tree.py
@@ -0,0 +1,20 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+        diameter = 0
+
+        def depth(root):
+            nonlocal diameter
+            if root is None:
+                return 0
+            left_depth = depth(root.left)
+            right_depth = depth(root.right)
+            diameter = max(diameter, left_depth + right_depth)
+            return 1 + max(left_depth, right_depth)
+        depth(root)
+        return diameter
