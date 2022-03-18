6984ace0add73cb81fef65507eccc0f34bfe8e43
Ozaki Yoichi <zak@inl.ics.keio.ac.jp>
2022-03-18 09:07:23 +0000

    leetcode

--- leetcode/606_construct_string_from_binary_tree.py
+++ leetcode/606_construct_string_from_binary_tree.py
@@ -6,6 +6,34 @@
 #         self.right = right
 class Solution:
     def tree2str(self, root: Optional[TreeNode]) -> str:
+        ans = ""
+
+        def rec(node):
+            nonlocal ans
+            if node is None:
+                return
+            ans += str(node.val)
+            if node.left is None and node.right is None:
+                return
+            ans += "("
+            rec(node.left)
+            ans += ")"
+            if node.right is not None:
+                ans += "("
+                rec(node.right)
+                ans += ")"
+        rec(root)
+        return ans
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def tree2str(self, root: Optional[TreeNode]) -> str:
         if root is None:
             return ""
 
