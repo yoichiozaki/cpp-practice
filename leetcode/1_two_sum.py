de5ba189483f91470122029b8634ae82d4d20db4
Ozaki Yoichi <zak@inl.ics.keio.ac.jp>
2022-03-19 06:43:47 +0000

    leetcode

--- leetcode/1_two_sum.py
+++ leetcode/1_two_sum.py
@@ -6,3 +6,13 @@ class Solution:
             if complement in table:
                 return [idx, table[complement]]
             table[num] = idx
+
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        memo = dict()
+        for idx, num in enumerate(nums):
+            remain = target - num
+            if remain in memo:
+                return [idx, memo[remain]]
+            memo[num] = idx
