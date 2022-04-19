e665daae68e61dd1e970a7ee47432a96d5dedead
zakimal <oza777you@gmail.com>
2022-04-19 21:43:26 +0900

    leetcode

--- leetcode/983_minimum_cost_for_tickets.py
+++ leetcode/983_minimum_cost_for_tickets.py
@@ -16,3 +16,23 @@ class Solution:
                 )
             else:
                 dp[day] = dp[day - 1]
+
+
+class Solution:
+    def mincostTickets(self, days: List[int], costs: List[int]) -> int:
+        cost = 0
+        last7 = []
+        last30 = []
+        for day in days:
+            while len(last7) != 0 and last7[0][0] + 7 <= day:
+                last7.pop(0)
+            while len(last30) != 0 and last30[0][0] + 30 <= day:
+                last30.pop(0)
+            last7.append((day, cost + costs[1]))
+            last30.append((day, cost + costs[2]))
+            cost = min(
+                cost + costs[0],
+                last7[0][1],
+                last30[0][1]
+            )
+        return cost
