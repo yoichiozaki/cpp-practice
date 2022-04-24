3348b17709f5d68b5ea2f272e39377cc54653646
zakimal <oza777you@gmail.com>
2022-04-24 14:17:26 +0900

    leetcode

---
+++ leetcode/107_binary_tree_level_order_traversal_2.py
@@ -0,0 +1,22 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def levelOrderBottom(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if root is None:
+            return []
+        ans = []
+        suspended = [root]
+        while len(suspended) != 0:
+            ans.append([n.val for n in suspended])
+            next_suspended = []
+            for n in suspended:
+                if n.left is not None:
+                    next_suspended.append(n.left)
+                if n.right is not None:
+                    next_suspended.append(n.right)
+            suspended = next_suspended
+        return ans[::-1]
