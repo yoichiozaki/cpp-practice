f7bad6f61a4b70b43bee08fc00f0fbb07f6259a1
zakimal <oza777you@gmail.com>
2022-05-11 22:17:46 +0900

    leetcode

---
+++ leetcode/1005_maximize_sum_of_array_after_k_negations.py
@@ -0,0 +1,8 @@
+class Solution:
+    def largestSumAfterKNegations(self, nums: List[int], k: int) -> int:
+        nums.sort()
+        i = 0
+        while i < len(nums) and i < k and nums[i] < 0:
+            nums[i] = -nums[i]
+            i += 1
+        return sum(nums) - (k - i) % 2 * min(nums) * 2
