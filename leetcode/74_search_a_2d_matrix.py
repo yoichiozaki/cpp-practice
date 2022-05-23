ffc780c64dec8262f1add853088ab7db8d0674cd
zakimal <oza777you@gmail.com>
2022-05-23 21:04:21 +0900

    leetcode

--- leetcode/74_search_a_2d_matrix.py
+++ leetcode/74_search_a_2d_matrix.py
@@ -14,3 +14,21 @@ class Solution:
             else:
                 high = mid - 1
         return False
+
+
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        H = len(matrix)
+        W = len(matrix[0])
+        low = 0
+        high = H * W - 1
+        while low <= high:
+            mid = (low + high) // 2
+            num = matrix[mid // W][mid % W]
+            if num == target:
+                return True
+            if num < target:
+                low = mid + 1
+            else:
+                high = mid - 1
+        return False
