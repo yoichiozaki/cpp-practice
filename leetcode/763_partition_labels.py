d8c2df7c3f18bb83686033f9b3b2a93f6e5ab728
zakimal <oza777you@gmail.com>
2022-10-01 22:43:22 +0900

    leetcode

--- leetcode/763_partition_labels.py
+++ leetcode/763_partition_labels.py
@@ -14,3 +14,25 @@ class Solution:
                 head = tail + 1
                 tail = tail + 1
         return ans
+
+    
+    # tail_candidateを毎回求めるのではなくて最初に計算しておくと少し効率が良くなる
+    # 前から順番に見ていくことで出来上がる配列は最大値が格納されることになるという事実はあたりまえだけど意外と盲点かもしれない
+    class Solution:
+        def partitionLabels(self, s: str) -> List[int]:
+            ans = []
+            head = 0
+            tail = 0
+            last_pos = [0 for _ in range(26)]
+            for pos, ch in enumerate(s):
+                last_pos[ord(ch) - ord('a')] = pos
+            for ptr in range(len(s)):
+                tail_candidate = last_pos[ord(s[ptr]) - ord('a')]
+                if tail_candidate < tail:
+                    continue
+                tail = max(tail, tail_candidate)
+                if ptr == tail:
+                    ans.append(tail - head + 1)
+                    head = tail + 1
+                    tail = tail + 1
+            return ans
