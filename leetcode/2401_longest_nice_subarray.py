96ea727346c98370861ecc8c48c6a8c058e4d435
zakimal <oza777you@gmail.com>
2022-10-01 21:50:15 +0900

    leetcode

--- leetcode/2401_longest_nice_subarray.py
+++ leetcode/2401_longest_nice_subarray.py
@@ -1,6 +1,6 @@
 # 数列が与えられる。その数列の連続する部分列のうち、その部分列の要素を二進数表記した時にどの桁も1がダブらないような部分列の最大の長さを求めよ
 # 連続する部分列 -> 尺取り法、累積和
-# bitwise-andの逆演算はbitwise-xor
+# bitwise-orの逆演算はbitwise-xor
 class Solution:
     def longestNiceSubarray(self, nums: List[int]) -> int:
         ans = 0
@@ -8,7 +8,7 @@ class Solution:
         accum = 0
         for right in range(len(nums)):
             while accum & nums[right] != 0: # 尺取り法
-                accum = accum ^ nums[left] # ここで累積and和からnums[left]を「減算」
+                accum = accum ^ nums[left] # ここで累積or和からnums[left]を「減算」
                 left += 1
             accum = accum | nums[right]
             ans = max(ans, right - left + 1)
